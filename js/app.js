677c81726ef28e14f48d9da3ecae0001cfc6b654
Leonardo Canela Almeida <leocanela1@yahoo.com.br>
2017-11-05 20:18:58 -0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,60 +1,40 @@
 
-  ons.bootstrap()
-  .controller('appController', function($scope) {
-// Quero apagar isto
-  $scope.dados = {
-    pretest: 0.1,
-    drgepretest: 0.38,
-    itupretest: 0.48,
-  };
+ons.bootstrap()
+.controller('appController', function($scope) {
 
-// Navegação pela lista de doenças
-$scope.irpara = function($index) {
-  console.log($scope.doencas[$index].nome);
-  nav.pushPage($scope.doencas[$index].nome+'.html', {data: {title: $scope.doencas[$index].nome, ppp: $scope.doencas[$index].pt}})
-}
-
-//Lista de Doenças
- $scope.doencas = [{
-      nome: 'Enxaqueca',
-      desc: 'Diferencie enxaqueca de outras cefaléias',
-      pt: 0.1
-    }, {
-      nome: 'DRGE',
-      desc: 'Diagnóstico clínico e exames complementares',
-      pt: 0.38
-    }, {
-    	nome: 'ITU',
-    	desc: 'Diagnóstico clínico de ITU',
-    	pt: 0.48
-    } 
-  ];
-//LRs de Enxaqueca
-  $scope.todos = [{
-      text: '4 Critérios POUNDing',
-      done: false,
-      lplus: 24
-    }, {
-      text: 'Nausea',
-      done: false,
-      lplus: 19
-    }, {
-      text: 'Fotofobia',
-      done: false,
-      lplus: 5.8
-    }, {
-      text: 'Fonofobia',
-      done: false,
-      lplus: 5.2
-    }, {
-      text: 'Piora com esforço',
-      done: false,
-      lplus: 3.8
-    }
-  ];
-
-//LRs de DRGE
-  $scope.drge = [{
+//Todos os dados das doenças
+$scope.tudo = [{
+  nomedoenca: 'Enxaqueca',
+  descdoenca: 'Diferencie enxaqueca de outras cefaléias',
+  nomepag: 'enxaqueca',
+  pretest: 0.1,
+  lrs: [{
+    text: '4 Critérios POUNDing',
+    done: false,
+    lplus: 24
+  }, {
+    text: 'Nausea',
+    done: false,
+    lplus: 19
+  }, {
+    text: 'Fotofobia',
+    done: false,
+    lplus: 5.8
+  }, {
+    text: 'Fonofobia',
+    done: false,
+    lplus: 5.2
+  }, {
+    text: 'Piora com esforço',
+    done: false,
+    lplus: 3.8
+  }]},
+  {
+    nomedoenca: 'DRGE',
+    descdoenca: 'Diagnóstico clínico e exames complementares',
+    nomepag: 'drge',
+    pretest: 0.38,
+    lrs: [{
       text: 'Pirose e/ou Regurgitação',
       done: false,
       lplus: 2.83
@@ -70,114 +50,67 @@ $scope.irpara = function($index) {
       text: 'pHmetria de 24 horas',
       done: false,
       lplus: 9
-    }
-  ];
+    }]},
+    {
+      nomedoenca: 'ITU',
+      descdoenca: 'Diagnóstico clínico de ITU',
+      nomepag: 'itu',
+      pretest: 0.48,
+      lrs: [{
 
-  //LRs de ITU
-   $scope.itu = [{
-      text: 'Disúria e urgência',
-      done: false,
-      lplus: 3.47
-    }, {
-      text: 'Ausência de corrimento vaginal',
-      done: false,
-      lplus: 21.3
-    }, {
-      text: 'Nitrito',
-      done: false,
-      lplus: 10
-    }, 
-  ];
-$scope.nomepag = "ITU";
-
-//Calcula Posttest de Enxaqueca
-  $scope.getPost = function() {
-    var total = 0;
-    var posttest = $scope.dados.pretest
-    for (var i = 0; i < $scope.todos.length; i++) {
-      if ($scope.todos[i].done === true) {
-        var product = $scope.todos[i];
-        total += (product.lplus);
-        pretest = $scope.dados.pretest / (1 - $scope.dados.pretest);
-        pto = pretest * total;
-        posttest = pto / (1 + pto);
+        text: 'Disúria e urgência',
+        done: false,
+        lplus: 3.47
+      }, {
+        text: 'Ausência de corrimento vaginal',
+        done: false,
+        lplus: 21.3
+      }, {
+        text: 'Nitrito',
+        done: false,
+        lplus: 10
       }
 
-    }
-    return posttest * 100;
-  }
-// Calcula Posttest de DRGE
-  $scope.drgePost = function() {
-    var total = 0;
-    var posttest = $scope.dados.drgepretest
-    for (var i = 0; i < $scope.drge.length; i++) {
-      if ($scope.drge[i].done === true) {
-        var product = $scope.drge[i];
-        total += (product.lplus);
-        pretest = $scope.dados.drgepretest / (1 - $scope.dados.drgepretest);
-        pto = pretest * total;
-        posttest = pto / (1 + pto);
-      }
-    }
-    return posttest*100;
-  }
 
-  $scope.ituPost = function() {
-    var total = 0;
-    var posttest = $scope.dados.itupretest
-    for (var i = 0; i < $scope.itu.length; i++) {
-      if ($scope.itu[i].done === true) {
-        var product = $scope.itu[i];
-        total += (product.lplus);
-        pretest = $scope.dados.itupretest / (1 - $scope.dados.itupretest);
-        pto = pretest * total;
-        posttest = pto / (1 + pto);
-      }
-    }
-    return posttest*100;
+      ]}
+
+
+      }]
+
+      $scope.paginaatual = {};
+
+// Navegação pela lista de doenças
+$scope.irpara = function($index) {
+  $scope.paginaatual = $scope.tudo[$index].nomepag
+  console.log($scope.tudo[$index].nomepag);
+  nav.pushPage($scope.tudo[$index].nomepag+'.html', {data: {title: $scope.tudo[$index].nomedoenca}})
+  $scope.paginaatual = {
+    nomepag: $scope.tudo[$index].nomepag, 
+    nomedoenca: $scope.tudo[$index].nomedoenca,
+    descdoenca: $scope.tudo[$index].descdoenca,
+    pretest: $scope.tudo[$index].pretest,
+    lrs: $scope.tudo[$index].lrs,
   }
+}
+
+//Calcula Pos-Teste
+$scope.getPost = function() {
+  var total = 0;
+  var posttest = $scope.paginaatual.pretest
+  for (var i = 0; i < $scope.paginaatual.lrs.length; i++) {
+    if ($scope.paginaatual.lrs[i].done === true) {
+      var product = $scope.paginaatual.lrs[i];
+      total += (product.lplus);
+      pretest = $scope.paginaatual.pretest / (1 - $scope.paginaatual.pretest);
+      pto = pretest * total;
+      posttest = pto / (1 + pto);
+    }
+
+  }
+  return posttest * 100;
+}
+
+
+
 
 });
-
-/* Outros
-  }
-  $scope.archive = function() {
-    var oldTodos = $scope.todos;
-    $scope.todos = [];
-    angular.forEach(oldTodos, function(todo) {
-      if (!todo.done) $scope.todos.push(todo);
-    });
-  };
-
-
-
-  $scope.addTodo = function() {
-    $scope.todos.push({
-      text: $scope.todoText,
-      done: false,
-      lplus: $scope.lplus
-    });
-    $scope.todoText = '';
-    $scope.id = $scope.id++;
-  };
-
-  $scope.addTodo = function() {
-    $scope.todos.push({
-      text: $scope.todoText,
-      done: false,
-      lplus: $scope.lplus
-    });
-    $scope.todoText = '';
-    $scope.id = $scope.id++;
-  };
-
-
-
-  $scope.remaining = function() {
-    var count = 0;
-    angular.forEach($scope.todos, function(todo) {
-      count += todo.done ? 0 : 1;
-    });
-    return count;
-  };
-*/
